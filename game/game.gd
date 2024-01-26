d7c5f414c49c709a47738e8277ad3b4214c49ca7
Jeffrey Bissel <jeffrey@bissel.it>
2024-01-26 20:15:54 +0100

    Added Cat-Tree to level selection

--- game/game.gd
+++ game/game.gd
@@ -33,7 +33,7 @@ func _open_level(level: MainState.MiniGames):
 		MainState.MiniGames.WINDOW:
 			state.set_level(level, assets.spawn_level(self, assets.template_level_window))
 		MainState.MiniGames.CAT_TREE:
-			state.set_level(level, assets.spawn_level(self, assets.template_level_window))
+			state.set_level(level, assets.spawn_level(self, assets.template_level_cat_tree))
 		MainState.MiniGames.COMPUTER:
 			state.set_level(level, assets.spawn_level(self, assets.template_level_window))
 		MainState.MiniGames.KITCHEN_COFFEE:
