85f12485fb69ae5c82539086891812dd9e4dd14e
Jeffrey Bissel <jeffrey@bissel.it>
2024-01-27 02:40:49 +0100

    mvp moorhuhn

--- game/level/moorhuhn/bird.gd
+++ game/level/moorhuhn/bird.gd
@@ -1,21 +1,26 @@
 class_name Bird extends Node2D
 
-enum State { FLYING, KILLED, FREE }
+enum State { INIT, FLYING, KILLED, FREE, DELETABLE }
 
 # left to right
 var movement = Vector2(4.0, -1.0)
-var state = State.FLYING
+var state = State.INIT
 
 func set_start():
+	state = State.INIT
 	var viewport = get_viewport()	
 	var start_pos = Vector2(-viewport.size.x * 0.5 + 50.0, viewport.size.y * 0.5 - 50.0)
 	
 	set_position(start_pos)
+	state = State.FLYING
 	
 func move():
 	set_position(position + movement)
-	
 	_check_edge()
+	
+func killed():
+	state = State.KILLED
+	movement = Vector2(0.0, 10.0)
 	
 func _check_edge():
 	var viewport = get_viewport().size * 0.5
@@ -25,7 +30,29 @@ func _check_edge():
 	
 	if position.x > viewport.x:
 		movement.x = -movement.x
+		
+	if state == State.KILLED or state == State.FREE:
+		if position.y < -viewport.y - 400 or position.y > viewport.y + 400:
+			state = State.DELETABLE
+
+# Hitpints 0 = Not Hit
+#          3 = Bulls Eye 
+func was_hit(pos: Vector2) -> int:
+	if state != State.FLYING:
+		return 0
+		
+	var dist = (position - pos).length()
 	
+	if dist < 15:
+		return 3
+	
+	if dist < 30:
+		return 2
+		
+	if dist < 100:
+		return 1
+		
+	return 0
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- game/level/moorhuhn/moorhuhn.gd
+++ game/level/moorhuhn/moorhuhn.gd
@@ -5,32 +5,63 @@ var template_bird = preload("res://level/moorhuhn/bird.tscn")
 
 var bpm = 10.0
 var rng = RandomNumberGenerator.new()
+var points = 0
 
 var birds = [] as Array[Bird]
 var next_bird_spawn_in_s = 0.0
+var max_birds = 10
+var max_points = 5
+var bird_holder: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super._ready()
+	bird_holder = get_node('./BirdHolder')
 	Input.set_custom_mouse_cursor(cursor_paw, 0, Vector2(64, 64))
 
 func _physics_process(delta):
 	next_bird_spawn_in_s = next_bird_spawn_in_s - delta;
 	
-	if next_bird_spawn_in_s < 0.0:
+	if next_bird_spawn_in_s < 0.0 and birds.size() < max_birds:
 		next_bird_spawn_in_s = ((1 / bpm) + rng.randf_range(-0.02, 0.02)) * 60.0
-		var bird = game.assets.spawn_new(self.get_node('./BirdHolder'), template_bird)
+		var bird = game.assets.spawn_new(bird_holder, template_bird)
 		bird.set_start()
 		birds.push_back(bird)
 		
 	for bird in birds:
 		bird.move()
+		
+	_remove_unused_birds()
+
+func _remove_unused_birds():
+	var toDel = []
 	
+	for bird in birds:
+		if bird.state == Bird.State.DELETABLE:
+			toDel.push_back(bird)
+	
+	for bird in toDel:
+		birds.erase(bird)
+		bird_holder.remove_child(bird)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if points >= max_points:
+		complete()
 	
-	
-	
-	
-	pass
+func _input(event):
+	if !event is InputEventMouseButton:
+		return
+		
+	if event.button_index != MOUSE_BUTTON_LEFT or !event.pressed:
+		return
+		
+	print("Left button was clicked at ", event.position)
+			
+	var pos = event.position - get_viewport().size * 0.5
+	for bird in birds:
+		var precision = bird.was_hit(pos)
+		if precision > 0:
+			bird.killed()
+			points += precision
+			print("points: ", points)
