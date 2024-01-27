0bc9d474a41efe74f413bcad1266d947db0cce48
Johannes Ganser <johannes.ganser@zarm.uni-bremen.de>
2024-01-27 22:21:24 +0100

    beautification

--- game/level/coffee/coffee.gd
+++ game/level/coffee/coffee.gd
@@ -1,15 +1,24 @@
 extends level_base
 
-var cursor_paw = load("res://assets/tiny_paw.png")
+var cursor_paw = load("res://level/coffee/sprites/drawn_paw.png")
+var cursor_dark_paw = load("res://level/coffee/sprites/dark_paw.png")
 var empty_cup = load("res://level/coffee/icons/coffee_cup_empty.png")
 var full_cup = load("res://level/coffee/icons/coffee_cup_full.png")
+var button_left = load("res://level/coffee/icons/button_left.png")
+var button_down = load("res://level/coffee/icons/button_down.png")
+var button_right = load("res://level/coffee/icons/button_right.png")
+var button_dict = {
+	1 : button_right,
+	2 : button_down,
+	3 : button_left
+}
 var full = false
 var poaring = false
 var clickable = true
 
 # Initial States
 var beans = 2
-var bar = 15
+var bar = 2
 var type = 3
 var times = ["12:36 PM","12:41 PM","12:59 PM","01:00 PM","01:15 PM"]
 var cur_time = 0
@@ -19,13 +28,13 @@ That's exactly how I like my coffee!!
 var neg_response = """No that's not the right time for this coffee.
 	:-("""
 var cup_positions = [
-	Vector2(1062, 667), # 0 -- right edge
-	Vector2( 883, 667), # 1
-	Vector2( 800, 667), # 2
-	Vector2( 720, 667), # 3
-	Vector2( 664, 667), # 4 -> goal_pos
-	Vector2( 560, 667), # 5
-	Vector2( 447, 667), # 6 -- left edge
+	Vector2(1062, 700), # 0 -- right edge
+	Vector2( 883, 700), # 1
+	Vector2( 800, 700), # 2
+	Vector2( 720, 700), # 3
+	Vector2( 664, 700), # 4 -> goal_pos
+	Vector2( 560, 700), # 5
+	Vector2( 447, 700), # 6 -- left edge
 ]
 var cur_pos = 0
 var goal_pos = 4
@@ -33,14 +42,17 @@ var goal_pos = 4
 var cat_face
 var count_cat_faces = 4
 
+func set_paw():
+	Input.set_custom_mouse_cursor(cursor_paw, 0, Vector2(63,63))
+
+func set_dark_paw():
+	Input.set_custom_mouse_cursor(cursor_dark_paw, 0, Vector2(63,63))
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super._ready()
-	$CanvasLayer/Control/GridContainer/type_label.text = str(type)
-	$CanvasLayer/Control/GridContainer/bean_label.text = str(beans)
-	$CanvasLayer/Control/GridContainer/bar_label.text = str(bar)
 	$CanvasLayer/Control/display/clock.text = times[cur_time]
-	Input.set_custom_mouse_cursor(cursor_paw, 0, Vector2(32, 32))
+	set_paw()
 	#cat_face = $CanvasLayer/Control/coffee_cup/AnimatedSprite2D
 	$CanvasLayer/Control/coffee_cup.set_position(cup_positions[cur_pos])
 
@@ -67,20 +79,21 @@ func _on_pour_coffee_button_pressed():
 
 func _on_adjust_bar_button_pressed():
 	click_button()
-	if bar == 15: bar = 20
-	elif bar == 20: bar = 10
-	elif bar == 10: bar = 15
-	$CanvasLayer/Control/GridContainer/bar_label.text = str(bar)
+	bar = (bar % 3) + 1
+	print(bar)
+	$CanvasLayer/Control/adjust_bar_button.set_texture_normal(button_dict[bar])
 
 func _on_adjust_beans_button_pressed():
 	click_button()
 	beans = (beans % 3) + 1
-	$CanvasLayer/Control/GridContainer/bean_label.text = str(beans)
+	print(beans)
+	$CanvasLayer/Control/adjust_beans_button.set_texture_normal(button_dict[beans])
 
 func _on_adjust_type_button_pressed():
 	click_button()
 	type = (type % 3) + 1
-	$CanvasLayer/Control/GridContainer/type_label.text = str(type)
+	print(type)
+	$CanvasLayer/Control/adjust_type_button.set_texture_normal(button_dict[type])
 
 
 func _on_display_pressed():
@@ -89,6 +102,8 @@ func _on_display_pressed():
 
 
 func _on_coffee_cup_pressed():
+	if poaring: return
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	cur_pos = (cur_pos + 1) % cup_positions.size()
 	if cur_pos == 0:
 		#$CanvasLayer/Control/coffee_cup.set_texture_normal(empty_cup)
@@ -98,6 +113,7 @@ func _on_coffee_cup_pressed():
 
 func _on_animated_sprite_2d_animation_finished():
 	print("finished poaring coffee :P")
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	if cur_pos != goal_pos:
 		return
 	#$CanvasLayer/Control/coffee_cup.set_texture_normal(full_cup)
@@ -135,3 +151,39 @@ func _on_background_finished():
 
 func _on_winning_purring_finished():
 	$winning_purring.play()
+
+func _on_adjust_type_button_mouse_entered():
+	set_dark_paw()
+
+func _on_adjust_type_button_mouse_exited():
+	set_paw()
+
+func _on_adjust_beans_button_mouse_entered():
+	set_dark_paw()
+
+func _on_adjust_beans_button_mouse_exited():
+	set_paw()
+
+func _on_adjust_bar_button_mouse_entered():
+	set_dark_paw()
+
+func _on_adjust_bar_button_mouse_exited():
+	set_paw()
+
+func _on_pour_coffee_button_mouse_entered():
+	set_dark_paw()
+
+func _on_pour_coffee_button_mouse_exited():
+	set_paw()
+
+func _on_coffee_cup_mouse_entered():
+	set_dark_paw()
+
+func _on_coffee_cup_mouse_exited():
+	set_paw()
+
+func _on_display_mouse_entered():
+	set_dark_paw()
+
+func _on_display_mouse_exited():
+	set_paw()
