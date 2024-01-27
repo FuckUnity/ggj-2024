932e5aa92fc92efe6a7a1ee66cdf2045d47dd91d
Johannes Ganser <johannes.ganser@zarm.uni-bremen.de>
2024-01-27 22:23:04 +0100

    remove unused reference to $.. -> bubble

--- game/level/coffee/speech_bubble.gd
+++ game/level/coffee/speech_bubble.gd
@@ -2,12 +2,10 @@ extends TextureRect
 
 var MAX_VISIBLE = 2.5
 var count_visible = 0.0
-var bubble
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	bubble = $".."
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
