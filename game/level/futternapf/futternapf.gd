49bf97db9a8f42bf46a4b4a1ce089535e70f4992
Luca Nixdorf <luca@familie-nixdorf.de>
2024-01-28 11:41:25 +0100

    futternapf game v1

--- game/level/futternapf/futternapf.gd
+++ game/level/futternapf/futternapf.gd
@@ -1,49 +1,72 @@
-extends Node2D
+extends level_base
 
 const dialogTree = [
 	{
-		'Q': "test",
-		'1': '2',
-		'2': '1',
-		'3': '1'
+		'Q': "Are you hungry?",
+		'a1': 2,
+		'a2': 1,
+		'a3': 1
 	},
 	{
-		'Q': "test2",
-		'1': '3',
-		'2': '2',
-		'3': '1'
+		'Q': "You cant be hungry, I just fed you!",
+		'a1': 3,
+		'a2': 2,
+		'a3': 1
 	},
 	{
-		'Q': "test2",
-		'1': '1',
-		'2': '1',
-		'3': '4'
+		'Q': "Wann play?",
+		'a1': 1,
+		'a2': 1,
+		'a3': 4
 	},
 	{
-		'Q': "test2",
-		'1': '3',
-		'2': '3',
-		'3': '5'
+		'Q': "Don't wanna play?",
+		'a1': 3,
+		'a2': 3,
+		'a3': 5
 	},
 	{
-		'Q': "test2",
-		'1': '3',
-		'2': '2',
-		'3': '6'
+		'Q': "Wanna go outside?",
+		'a1': 3,
+		'a2': 2,
+		'a3': 6
 	},
 	{
-		'Q': "test2",
-		'1': '7',
-		'2': '4',
-		'3': '1'
+		'Q': "Don't wanna go outside?",
+		'a1': 7,
+		'a2': 4,
+		'a3': 1
+	},
+	{
+		'Q': "Don't wanna go outside,\nDon't wanna play,\nCant help you!",
+		'a1': 8,
+		'a2': 8,
+		'a3': 8
 	}
 ]
 
+var current_level = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_init_q(1)
 
+func _init_q(nr):
+	if nr == 8:
+		complete()
+		return
+	nr -= 1 # index ist 0 based
+	current_level = nr
+	$contanier/Q.text = dialogTree[nr].Q
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_a_1_pressed():
+	var next = dialogTree[current_level].a1
+	_init_q(next)
+
+func _on_a_2_pressed():
+	var next = dialogTree[current_level].a2
+	_init_q(next)
+
+func _on_a_3_pressed():
+	var next = dialogTree[current_level].a3
+	_init_q(next)
